820e715be476536b10ba7637e586c279fd8d7910
Matias-Estay <matias.estay@hotmail.com>
2021-11-28 20:20:40 -0300

    Proyecto completo

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <layout>
+    <template v-slot:default>
+      <b-row class="justify-content-center">
+        <b-img responsive alt="Vue logo" src="~/assets/images/logo_pikachu.png" fluid class="ml-5" style="width:600px;"/>
+      </b-row>
+      <b-row class="justify-content-center"> 
+        <h3 style="text-align:center;">
+          Welcome to the pokédex
+        </h3>
+      </b-row>
+      <b-row class="justify-content-center">
+        <p class="text-justify text-center">
+          The digital encyclopedia created by Professor Oak is an invaluable tool 
+          to Trainers in the Pokémon world.
+        </p>
+      </b-row>
+      <b-row class="justify-content-center">
+        <NuxtLink class="poke-btn btn" to="/home">Get Started</NuxtLink>
+      </b-row>
+    </template>
+  </layout>
+</template>
+<script>
+export default {
+  name: 'app',
+}
+</script>
